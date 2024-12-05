74b0f7bfb92b8012b575ca5d9d120a54df450e2d
Hania Szlęk <84408598+haniaszlek@users.noreply.github.com>
2024-12-05 12:40:12 +0100

    Dodano wyswietlanie pierwszych 100 liczb

--- pierwsza.py
+++ pierwsza.py
@@ -3,3 +3,12 @@ def isPrime(n):
         if n%i==0:
             return False
     return True
+def first_10_primes():
+    licznik=0
+    n=1
+    while liczniik<100:
+        n+=1
+        if isPrime(n):
+            licznik+=1
+            print(n)
+    
